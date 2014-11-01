4cff5b93c59f4baa1411da2d2d5354d85d922662
Dylan Griffith <dyl.griffith@gmail.com>
2014-11-01 11:44:03 +1100

    dont put _spec on shared examples

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -3,6 +3,7 @@ $LOAD_PATH.unshift(File.join(File.dirname(__FILE__), "..", "lib"))
 require 'rspec'
 require 'pry'
 require 'spec_methods'
+require 'em_bucketer_examples'
 
 RSpec.configure do |config|
   config.order = :rand
